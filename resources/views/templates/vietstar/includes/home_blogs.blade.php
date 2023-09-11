efa7cbb61f0d8abd28800185ba5f5e91fc10fab6
minhdev <minhdev@gmail.com>
2023-09-11 15:03:52 +0700

    Comment

--- resources/views/templates/vietstar/includes/home_blogs.blade.php
+++ resources/views/templates/vietstar/includes/home_blogs.blade.php
@@ -9,7 +9,7 @@
         </div> 
         <div class="swiper sectionBlock__swiper">
             <div class="swiper-wrapper">
-                
+                <!-- Start Render data of  Blog here -->
                             <div class="swiper-slide">
                                 <div class="cardBlock">
                                     <a class="cardBlock_img" href="#">
@@ -111,61 +111,11 @@
                                 </div>
                             </div>
                        
-                      
-                <!-- <div class="swiper-slide">Slide 2</div>
-                <div class="swiper-slide">Slide 3</div>
-                <div class="swiper-slide">Slide 4</div>
-                <div class="swiper-slide">Slide 5</div>
-                <div class="swiper-slide">Slide 6</div>
-                <div class="swiper-slide">Slide 7</div>
-                <div class="swiper-slide">Slide 8</div>
-                <div class="swiper-slide">Slide 9</div> -->
+                     <!-- End Render data of  Blog here --> 
             </div>
         </div>
     </div>
 </section>
-
-
-
-
-      <!-- <h2 class="section-title">{{ __('Blog') }}</h2> -->
-      <!-- <div class="__row justify-content-between">
-        <div class="card-list d-flex flex-row gap-24">
-            <div class="swiper swiper-blogs-slider">
-                <div class="swiper-wrapper">
-                    @if(null!==($blogs))
-                        @php
-                        $count = 1;
-                        @endphp
-                        @foreach($blogs as $blog)
-                        @php
-                        $cate_ids = explode(",", $blog->cate_id);
-                        $data = DB::table('blog_categories')->whereIn('id', $cate_ids)->get();
-                        $cate_array = array();
-                        foreach ($data as $cat) {
-                            $cate_array[] = "<a href='" . url('/blog/category/') . "/" . $cat->slug . "'>$cat->heading</a>";
-                        }
-                        @endphp
-                        <div class="swiper-slide">
-                            <div class="card-list-item">
-                                <div class="card-list-item__img">
-                                    <img src="{{asset('uploads/blogs/'.$blog->image)}}"
-                                    alt="{{$blog->heading}}">
-                                </div>
-                                <div class="card-list-item__title"><a href="{{route('blog-detail',$blog->slug)}}">{{$blog->heading}}</a></div>
-                            </div>
-                        </div>
-                        @php($count++)
-                        @endforeach
-                    @endif
-                </div>
-            </div>
-        </div> -->
-        <!-- button -->
-        <!-- <div class="d-flex align-self-center">
-          <a class="btn btn-viewall-post"  href="{{ route('blogs') }} {{ Request::url() == route('blogs') ? 'active' : '' }}" ><span>{{__('View All Blog Posts')}}</span> <i
-              class="fas fa-arrow-right ms-2"></i></a>
-        </div> -->
 
 
 

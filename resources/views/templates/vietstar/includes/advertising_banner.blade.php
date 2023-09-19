e025a9612d2e8c7038b83669f802c9500cea46e5
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-09-19 09:35:31 +0700

    change contact page

--- resources/views/templates/vietstar/includes/advertising_banner.blade.php
+++ resources/views/templates/vietstar/includes/advertising_banner.blade.php
@@ -1,4 +1,3 @@
-
 <section class="advertising-banner cb-section cb-section-border-bottom">
     <div class="container">
         <div class="row">
@@ -6,25 +5,7 @@
                 <div class="item">
                     <div class="image loadAds">
                         <a href="http://localhost:8000/">
-                            <img src="https://ads.careerbuilder.vn/www/images/a5b2628391fac3d894caa7e1a29d12fa.jpg" alt="#">
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="item">
-                    <div class="image loadAds">
-                    <a href="http://localhost:8000/">
-                            <img src="https://ads.careerbuilder.vn/www/images/40322bfe0026051c3092d837b500e854.jpg" alt="#">
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-lg-3">
-                <div class="item">
-                    <div class="image loadAds">
-                    <a href="http://localhost:8000/">
-                            <img src="https://ads.careerbuilder.vn/www/images/cd36bdd64ecdebbeeafc6347057ee992.png" alt="#">
+                            <img src="https://elca.vietnamworks.com/assets/images/page/banner/cover.png?r=1689852315" alt="#">
                         </a>
                     </div>
                 </div>
@@ -33,11 +14,29 @@
                 <div class="item">
                     <div class="image loadAds">
                         <a href="http://localhost:8000/">
-                            <img src="https://ads.careerbuilder.vn/www/images/b818531cf76fdafbf772e3d95f5f102a.png" alt="#">
+                            <img src="https://elca.vietnamworks.com/assets/images/page/banner/cover.png?r=1689852315" alt="#">
                         </a>
                     </div>
                 </div>
-            </div> 
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="item">
+                    <div class="image loadAds">
+                        <a href="http://localhost:8000/">
+                            <img src="https://elca.vietnamworks.com/assets/images/page/banner/cover.png?r=1689852315" alt="#">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="item">
+                    <div class="image loadAds">
+                        <a href="http://localhost:8000/">
+                            <img src="https://elca.vietnamworks.com/assets/images/page/banner/cover.png?r=1689852315" alt="#">
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>

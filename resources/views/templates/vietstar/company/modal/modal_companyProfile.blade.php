9ca517e4d18deec3e8da481c6045c72db4414c4c
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-10-12 15:40:41 +0700

    Revert "update modal user info"
    This reverts commit c14e87159ab45490f0e532f671a4d97707b48997.

--- resources/views/templates/vietstar/company/modal/modal_companyProfile.blade.php
+++ resources/views/templates/vietstar/company/modal/modal_companyProfile.blade.php
@@ -175,6 +175,10 @@ $company = Auth::guard('company')->user();
 </section>
 
       </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+       
+      </div>
     </div>
   </div>
 </div>

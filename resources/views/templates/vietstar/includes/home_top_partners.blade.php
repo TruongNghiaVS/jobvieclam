4e11b090450c5b66fa63dff86ffc8851cc8047aa
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-10-20 11:09:40 +0700

    check home page

--- resources/views/templates/vietstar/includes/home_top_partners.blade.php
+++ resources/views/templates/vietstar/includes/home_top_partners.blade.php
@@ -1,5 +1,4 @@
 <section class="partner cb-section pt-0">
-
     <div class="container">
         <div class="box-title-bg">
             <h2 class="section-title text-uppercase">{{ __('Nhà tuyển dụng hàng đầu') }}</h2>
@@ -14,15 +13,14 @@
                 <?php
                 $company = App\Company::where('id', '=', $company_id_num_jobs->company_id)->active()->first();
                 if (null !== $company) { ?>
-                <div class="swiper-slide">
-                    <div class="partner-item__box">
-                        <a alt="1 slide" href="{{route('company.detail', $company->slug)}}" title="{{$company->name}}">
-                            <img src="{{ asset('company_logos/'.$company->logo)}}" class="partner-item__img"
-                                alt="1 slide">
-                        </a>
-                        <h3 class="partner-item__name">Công ty cổ phần tập đoàn Vietstar</h3>
+                    <div class="swiper-slide">
+                        <div class="partner-item__box">
+                            <a alt="1 slide" href="{{route('company.detail', $company->slug)}}" title="{{$company->name}}">
+                                <img src="{{ asset('company_logos/'.$company->logo)}}" class="partner-item__img" alt="1 slide">
+                            </a>
+                            <h3 class="partner-item__name">Công ty cổ phần tập đoàn Vietstar</h3>
+                        </div>
                     </div>
-                </div>
                 <?php } ?>
                 @endforeach
                 @endif

615b29c86fccd2880a8f820e84161bf924ef1516
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-10-26 13:16:31 +0700

    check css change public profile

--- resources/views/templates/employers/includes/candidate_profile_modal.blade.php
+++ resources/views/templates/employers/includes/candidate_profile_modal.blade.php
@@ -112,7 +112,7 @@
                         @php
                             $degree_type = ($education->Industries) ? ' | '.$education->Industries->industry : '';
                         @endphp
-                        <p class="text-blue-color mb-2">{{$education->date_completion}} | {{$education->institution . $degree_type}}</p>
+                        <p class="text-primary mb-2 font-weight-bold">{{$education->date_completion}} | {{$education->institution . $degree_type}}</p>
                         <h6 class="my-0">{{$education->degree}}</h6>
                         <p>{{$education->school}}</p>
                     </div>
@@ -125,7 +125,7 @@
                 <h6>{{__('Skills')}}</h6>
                 @foreach($user->profileSkills as $skill)
                     <div class="form-card d-flex justify-content-between mb-3">
-                        <p class="text-blue-color mb-2">{{ $jobSkills[$skill->job_skill_id] ?? ''}}</p>
+                        <p class="text-primary mb-2 font-weight-bold">{{ $jobSkills[$skill->job_skill_id] ?? ''}}</p>
                         <p>{{$jobExperiences[$skill->job_experience_id] ?? ''}} {{$skill->job_experience_id}}/5</p>
                     </div>
                 @endforeach
@@ -136,7 +136,7 @@
                 <h6>{{__('Languages')}}</h6>
                 @foreach($user->profileLanguages as $language)
                     <div class="form-card d-flex justify-content-between mb-3">
-                        <p class="text-blue-color mb-2"> {{$jobLanguages[$language->language_id]?? ''}}</p>
+                        <p class="text-primary mb-2 font-weight-bold"> {{$jobLanguages[$language->language_id]?? ''}}</p>
                         <p>{{$jobLanguageLevels[$language->language_level_id] ?? ''}}</p>
                     </div>
                 @endforeach

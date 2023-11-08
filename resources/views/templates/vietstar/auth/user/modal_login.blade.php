a6e7e68a381121e2c796e3149711ec91b8c0bb10
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-11-08 14:43:43 +0700

    login success

--- resources/views/templates/vietstar/auth/user/modal_login.blade.php
+++ resources/views/templates/vietstar/auth/user/modal_login.blade.php
@@ -62,6 +62,25 @@
 </div>
 
 
+<div class="modal fade" id="login_success" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label=""><span>×</span></button>
+                     </div>
+					
+                    <div class="modal-body">
+                     
+                        <div class="thank-you-pop">
+							<img src="http://goactionstations.co.uk/wp-content/uploads/2017/03/Green-Round-Tick.png" alt="">
+							<h1>{{__('Sign Up Success')}}</h1>		
+ 						</div>
+                    </div>
+					
+        </div>
+    </div>
+</div>
+
 @push('styles')
 <style>
     .invalid-feedback {
@@ -75,6 +94,64 @@
     .form-control.has-error{
         border: 1px solid #dc3545 !important;
     }
+    #login_success.show {
+        display: block;
+        height: 100%;
+    }
+    .thank-you-pop{
+	width:100%;
+ 	padding:20px;
+	text-align:center;
+}
+.thank-you-pop img{
+	width:76px;
+	height:auto;
+	margin:0 auto;
+	display:block;
+	margin-bottom:25px;
+}
+
+.thank-you-pop h1{
+	font-size: 42px;
+    margin-bottom: 25px;
+	color:#5C5C5C;
+}
+.thank-you-pop p{
+	font-size: 20px;
+    margin-bottom: 27px;
+ 	color:#5C5C5C;
+}
+.thank-you-pop h3.cupon-pop{
+	font-size: 25px;
+    margin-bottom: 40px;
+	color:#222;
+	display:inline-block;
+	text-align:center;
+	padding:10px 20px;
+	border:2px dashed #222;
+	clear:both;
+	font-weight:normal;
+}
+.thank-you-pop h3.cupon-pop span{
+	color:#03A9F4;
+}
+.thank-you-pop a{
+	display: inline-block;
+    margin: 0 auto;
+    padding: 9px 20px;
+    color: #fff;
+    text-transform: uppercase;
+    font-size: 14px;
+    background-color: #8BC34A;
+    border-radius: 17px;
+}
+.thank-you-pop a i{
+	margin-right:5px;
+	color:#fff;
+}
+#login_success .modal-header{
+    border:0px;
+}
 
 </style>
 @endpush
@@ -174,10 +251,13 @@ $(document).ready(function() {
                 }
                 })
                 .done(function(data){
-                    // setTimeout(function() { 
-                    //     alert(data.message)
-                    //     window.location.href = data.urlRedirect;
-                    // }, 2000);
+                    $("#login_success").addClass("show")
+                    $("#user_login_Modal").css("display:none")
+                    $("#user_login_Modal").removeClass("show")
+                    $("#login_success button").click(function(){
+                        $("#login_success").removeClass("show")
+                        window.location.href =  "/home";
+                    });
                     
                 })
                 .fail(function(jqXHR, textStatus){

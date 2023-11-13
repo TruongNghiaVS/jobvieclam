7fcb5f468f372dea356cc19254fe788da2c9d947
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-11-13 15:19:47 +0700

    .

--- resources/views/templates/vietstar/includes/header.blade.php
+++ resources/views/templates/vietstar/includes/header.blade.php
@@ -197,8 +197,8 @@
 
 
 
-                <!-- <a class="btn_for_em" href="{{route('employerIndex')}}"> -->
-                <a class="btn_for_em" href="http://127.0.0.1:8001/employers">
+                <a class="btn_for_em" href="{{route('employerIndex')}}">
+                <!-- <a class="btn_for_em" href="http://127.0.0.1:8001/employers"> -->
 
                 <div  class="btn_for_em__head">
                     Dành cho

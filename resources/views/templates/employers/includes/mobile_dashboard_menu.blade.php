c70b203b7a18f40946f78b7cc7c8d400882008b0
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-11-13 09:11:28 +0700

    change mobile url

--- resources/views/templates/employers/includes/mobile_dashboard_menu.blade.php
+++ resources/views/templates/employers/includes/mobile_dashboard_menu.blade.php
@@ -723,7 +723,8 @@ Dashboard
                 <a class="nav-link login_link btn btn-primary login-btn" data-toggle="modal" data-target="#employer_login_Modal" >{{__('Log in')}} / {{__('Đăng ký')}} </a>
                     {{--<a class="btn btn-primary my-2" href="{{route('register')}}" class="nav-link
                     register">{{__('Đăng ký')}}</a> --}}
-                    <a href="{{route('index')}}" class="btn btn-primary">Dành cho ứng viên</a>
+                    <!-- <a href="{{route('index')}}" class="btn btn-primary">Dành cho ứng viên</a> -->
+                    <a href="http://127.0.0.1:8000/" class="btn btn-primary">Dành cho ứng viên</a>
 
                     @if(Auth::guard('company')->user())
                     <a class="btn btn-primary my-2" href="{{route('job.seeker.list')}}" class="nav-link">{{__('Find candidates')}}</a>

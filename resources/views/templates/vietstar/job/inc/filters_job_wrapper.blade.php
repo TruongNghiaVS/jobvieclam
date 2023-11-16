3da64f809bf7b94e2c8885c213eb481c4b830fa5
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-11-16 16:51:32 +0700

    min max search

--- resources/views/templates/vietstar/job/inc/filters_job_wrapper.blade.php
+++ resources/views/templates/vietstar/job/inc/filters_job_wrapper.blade.php
@@ -1,10 +1,120 @@
 @push('styles')
 <style type="text/css">
+.select-menu {
+  max-width: 330px;
 
+}
+.select-menu .select-btn {
+  display: flex;
+ 
+  padding: 8px 16px;
+    border: 1px solid #ced4da;
+    border-radius: 4px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #212529;
+}
+.select-menu .options {
+  position: absolute;
+  width: 330px;
+  overflow-y: auto;
+  max-height: 295px;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 8px;
+  background: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  animation-name: fadeInDown;
+  -webkit-animation-name: fadeInDown;
+  animation-duration: 0.35s;
+  animation-fill-mode: both;
+  -webkit-animation-duration: 0.35s;
+  -webkit-animation-fill-mode: both;
+}
+.select-menu .options .option {
+  display: flex;
+  height: 55px;
+  cursor: pointer;
+  padding: 0 16px;
+  border-radius: 8px;
+  align-items: center;
+  background: #fff;
+}
+.select-menu .options .option:hover {
+  background: #f2f2f2;
+}
+.select-menu .options .option i {
+  font-size: 25px;
+  margin-right: 12px;
+}
+.select-menu .options .option .option-text {
+  font-size: 18px;
+  color: #333;
+}
+
+.select-btn i {
+  font-size: 25px;
+  transition: 0.3s;
+}
+
+.select-menu.active .select-btn i {
+  transform: rotate(-180deg);
+}
+.select-menu.active .options {
+  display: block;
+  opacity: 0;
+  z-index: 10;
+  animation-name: fadeInUp;
+  -webkit-animation-name: fadeInUp;
+  animation-duration: 0.4s;
+  animation-fill-mode: both;
+  -webkit-animation-duration: 0.4s;
+  -webkit-animation-fill-mode: both;
+}
+.box-field {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+
+}
+.box-field  input {
+    background-color: #fff!important;
+    border: 1px solid #e9eaec!important;
+    border-radius: 6px;
+    color: #000!important;
+    font-weight: 700;
+    height: 40px;
+    padding: 8px 12px;
+    width: 105.5px!important;
+}
+.custom-salary-option {
+    margin:10px 0;
+}
+@keyframes fadeInUp {
+  from {
+    transform: translate3d(0, 30px, 0);
+  }
+  to {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+}
+@keyframes fadeInDown {
+  from {
+    transform: translate3d(0, 0, 0);
+    opacity: 1;
+  }
+  to {
+    transform: translate3d(0, 20px, 0);
+    opacity: 0;
+  }
+}
 </style>
 @endpush
 {{--<?php
-dd($funclAreas)
+dd($salaryFroms)
 ?>--}}
 {!! Form::open(['method' => 'get','route' => 'job.list', 'id' => 'job_filter']) !!}
 <!-- SEARCH STICKY -->
@@ -73,12 +183,42 @@ dd($funclAreas)
         <div class="filters-wrapper">
             <form action="{{route('job.list')}}" method="get">
                 <div class="row">
-                    <div class="col-sm-6 col-lg-2">
-                        <div class="form-group form-select">
+                    <div class="form-group col-sm-6 col-lg-2">
+                        
                             <label>{{__('Salary Level')}}</label>
-                            {!! Form::select('salary_from',['' => __('Salary Level')] + $salaryFroms, Request::get('salary_from', null), array('class'=>'form-control form-select shadow-sm', 'id'=>'salary_from')) !!}
+       
+
+                            <div class="select-menu">
+                                <div class="select-btn">
+                                    <span class="sBtn-text">{{__('Salary Level')}}</span>
+                                    <i class="bx bx-chevron-down"></i>
+                                </div>
+
+                                <ul class="options">
+                                    <form id="custom-salary-form">
+                                        <li class="custom-salary-option">
+                                            <div class="">
+                                                <div class="box-field">
+                                                    <input type="number" name="salary_min" id="salary_min" placeholder="Từ" max="9999" data-gtm-form-interact-field-id="0">
+                                                    <span>-</span>
+                                                    <input type="number" name="salary_max"  id="salary_max" placeholder="Đến" max="9999" data-gtm-form-interact-field-id="1">
+                                                    <span>triệu</span>
+                                                </div>
+                                                <button type="button" class="btn btn-primary w-100 btn-custom-salary" disabled>Áp dụng</button>
+                                            </div>
+                                        </li>
+                                    </form>
+
+                                    @foreach ($salaryFroms as $key => $salaryFrom) 
+                                        <li class="option">
+                                            <span class="option-text" key="{{$key}}">{{$salaryFrom}}</span>
+                                        </li>
+                                    @endforeach
+                                    
+                                </ul>
+                        
                     
-                        </div>
+                            </div>
                     </div>
                     <div class="col-sm-6 col-lg-3">
                         <div class="form-group" id="degree_level_dd">
@@ -186,6 +326,31 @@ dd($funclAreas)
 {!! Form::close() !!}
 @push('scripts')
 <script type="text/javascript">
+$(document).ready(function() { 
+    $(document).ready(function() {
+        // Attach input event listeners
+        $('#salary_min, #salary_max').on('input', function() {
+            checkInputs(); // Check inputs whenever there is input
+        });
+    });
+
+    function checkInputs() {
+      
+        // Get the values of the inputs
+        var input1Value = $('#salary_min').val();
+        var input2Value = $('#salary_max').val();
+        // Enable the button if both inputs have values, otherwise disable it
+        if (input1Value && input2Value) {
+         
+            $('.btn-custom-salary').prop('disabled', false);
+        } else {
+            $('.btn-custom-salary').prop('disabled', true);
+        }
+    }
+
+    })
+
+
     $(document).ready(function() {
         $('#benefit_id').multiselect({
             texts: {
@@ -224,6 +389,25 @@ dd($funclAreas)
             
     });
 
+    const optionMenu = document.querySelector(".select-menu"),
+    selectBtn = optionMenu.querySelector(".select-btn"),
+    options = optionMenu.querySelectorAll(".option"),
+    sBtn_text = optionMenu.querySelector(".sBtn-text");
 
+    selectBtn.addEventListener("click", () =>
+    optionMenu.classList.toggle("active")
+    );
+
+    options.forEach((option) => {
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+
+        optionMenu.classList.remove("active");
+    });
+    });
+
+    
+    
 </script>
 @endpush

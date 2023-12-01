700a1649f92ddd15a2e7c95e2e476af947cf7832
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-12-01 15:22:41 +0700

    form reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -15,7 +15,7 @@
                 </div>
             </div>
             <div class="col-lg-6 col-md-6  col-sm-12" bis_skin_checked="1">
-                <div class="box-info-signup forgot-password" bis_skin_checked="1">
+                <div class="box-info-signup forgot-password" id="form_reset_content" bis_skin_checked="1">
                     <div class="title" bis_skin_checked="1">
                         <h2 class="text-primary">Quên Mật Khẩu</h2>
                     </div>
@@ -27,15 +27,18 @@
 
 
                     <div class="main-form" bis_skin_checked="1">
-                        <form class="form-horizontal" method="POST" >
+                        <form class="form-horizontal"   id="resetPasswordForm">
+                            {{ csrf_field() }}
                             <div class="form-group d-flex" bis_skin_checked="1">
                                 <div class="form-info" bis_skin_checked="1">
                                     <span>Email</span>
                                 </div>
                                 <div class="form-input" bis_skin_checked="1">
-                                    <input type="text" name="email" id="email" class="form-control" placeholder=" Vui lòng nhập thông tin" onkeyup="this.setAttribute('value', this.value);" value="" onfocus="javascript:if(this.value=='Email/Tên đăng nhập') this.value='';">
-                                    <span class="form-error error_email">
-                                    </span>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus placeholder="{{__('Email Address')}}">
+                             
+                             <div class="invalid-feedback email-error">
+                                 {{__('Email is required')}}
+                             </div>
                                 </div>
                             </div>
                             <div class="form-group d-flex" bis_skin_checked="1">
@@ -43,9 +46,10 @@
                                     <span>Mã xác nhận</span>
                                 </div>
                                 <div class="form-input" bis_skin_checked="1">
-                                    <input type="text" name="email" id="email" class="form-control" placeholder=" Vui lòng nhập thông tin" onkeyup="this.setAttribute('value', this.value);" value="" onfocus="javascript:if(this.value=='Email/Tên đăng nhập') this.value='';">
-                                    <span class="form-error error_email">
-                                    </span>
+                                <input type="text" class="form-control" id="verificationCode" required>
+                                        <div class="invalid-feedback code-error">
+                                            {{__('Please enter the verification code')}}
+                                        </div>
                                 </div>
 
                             
@@ -53,7 +57,7 @@
                             
                             <div class="user-action" bis_skin_checked="1">
                                 <div class="btn-area" bis_skin_checked="1">
-                                    <button type="submit" class="btn btn-primary" value="Gửi">Gửi</button>
+                                    <button type="button" id="resetPasswordBtn"  class="btn btn-primary" value="Gửi">Gửi</button>
                                 </div>
                                 <p> <a class="register" href="#" data-toggle="modal" data-target="#user_logup_Modal">Quý khách chưa có tài khoản?</a> Đăng ký dễ dàng, hoàn toàn miễn phí</p>
 
@@ -100,9 +104,9 @@
     }
 
     .box-info-signup .main-step-title h3 {
-        text-transform: uppercase;
+       
         font-weight: normal;
-        font-size: 22px;
+        font-size: 17px;
         color: #333;
     }
 
@@ -141,13 +145,95 @@
     }
 
     .user-action {
-        text-align: right;
+
     }
 
     .user-action>* {
         margin-bottom: 20px;
     }
+    .btn-area {
+        text-align: right;
+    }
 </style>
+@endpush
+
+
+@push('scripts')
+<script type="text/javascript">
+$('#resetPasswordBtn').on('click', function () {
+        // Get values from the form
+        var email = $('#resetPasswordForm #email').val();
+        var verificationCode = $('#resetPasswordForm #verificationCode').val();
+
+        // Validate form fields
+        $('.invalid-feedback').hide();
+
+        console.log(email);
+        // Validate form fields
+        if (!email) {
+            $('.email-error').show(); // Show email error message
+            
+        }
+
+        if (!verificationCode) {
+            $('.code-error').show(); // Show verification code error message
+            
+        }
+
+
+        if(email) {
+            $("#form_reset_content").empty();
+            $("#form_reset_content").append(`<div class="title" bis_skin_checked="1">
+                            <h2 class="text-primary">Quên Mật Khẩu</h2>
+                        </div>`)
+
+            $("#form_reset_content").append(`<div class="step-title d-flex align-center" bis_skin_checked="1">
+                            <div class="main-step-title" bis_skin_checked="1">
+                                <h3>Vui lòng kiểm tra email của bạn và làm theo hướng dẫn để tạo mật khẩu mới</h3>
+                            </div>
+                        </div>`)
+            $("#form_reset_content").append(`<div class="main-form"  bis_skin_checked="1"> <p class="my-3">
+                                    Nếu bạn sử dụng Gmail hoặc công ty bạn đang sử dụng dịch vụ email của Google để đăng ký tài khoản, bạn nên kiểm tra email trong các mục Inbox/Hộp thư đến (Primary, Social, Promotions) và Spam. Hoặc dùng công cụ tìm kiếm email để tìm tên email: support@Jobvieclam.
+                                </p></div>
+                                <div class="user-action" bis_skin_checked="1">
+                               
+                                <p> <a class="register" href="#" data-toggle="modal" data-target="#company_logup_Modal">Quý khách chưa có tài khoản?</a> Đăng ký dễ dàng, hoàn toàn miễn phí</p>
+
+                                <div class="text-help" bis_skin_checked="1">
+                                    <p>Nếu bạn cần sự trợ giúp, vui lòng liên hệ:</p>
+                                    <p>Email: <a href="#" target="_blank">support@jobvieclam.com</a></p>
+                                </div>
+                            </div>`)
+
+
+
+        }
+
+
+        // Perform AJAX request to check if the email exists
+        // $.ajax({
+        //     url: 'YOUR_SERVER_ENDPOINT',
+        //     type: 'POST',
+        //     data: {
+        //         email: email,
+        //         verificationCode: verificationCode
+        //     },
+        //     success: function (response) {
+        //         if (response === 1) {
+        //             // Email exists, you can proceed with password reset logic
+        //             alert('Password reset successful!');
+        //         } else {
+        //             // Email does not exist, show error message
+        //             $('.invalid-feedback').show();
+        //         }
+        //     },
+        //     error: function (xhr, status, error) {
+        //         // Handle error
+        //         alert('Error: ' + error);
+        //     }
+        // });
+    });
+</script>
 @endpush
 
 

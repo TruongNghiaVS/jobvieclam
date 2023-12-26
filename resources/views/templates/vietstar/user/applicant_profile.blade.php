b630481dd13a8b8ad59fd1279d5e8a3cf307f59a
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2023-12-26 17:07:03 +0700

    public_profile

--- resources/views/templates/vietstar/user/applicant_profile.blade.php
+++ resources/views/templates/vietstar/user/applicant_profile.blade.php
@@ -1,19 +1,7 @@
 
 <!-- Header end --> 
 <!-- Inner Page Title start --> 
-{{-- <?php $true = FALSE; ?>
 
-<?php 
-if(Auth::guard('company')->user()){
-$package = Auth::guard('company')->user();
-if(null!==($package)){
-    $array_ids = explode(',',$package->availed_cvs_ids);
-    if(in_array($user->id, $array_ids)){
-        $true = TRUE;
-    }
-}
-}
-?> --}}
 
 
 <?php 
@@ -28,168 +16,220 @@ if(null!==($package)){
 
 ?>
 
-<!-- Public profile cover -->
-<section class="public-profile-cover">
-    <div>
-      <img src="{{ asset('vietstar/imgs/company-cover.jpg') }}" alt="vietstar - company">
-    </div>
-</section>
-<section class="container public-profile">
-    <div class="card card-bio mb-5 w-100 shadow-sm">
-      <div class="row g-0">
-        <div class="col-md-2 img-bio rounded mx-md-auto mt-md-4 d-block">
-          <!-- <img src="{{ $user->avatar() }}" class="img-bio rounded mx-md-auto mt-md-4 d-block" alt="vietstar"> -->
-          {{ $user->printUserImage(150,150) }}
-        
-        
-        </div>
-        <div class="col-md-10">
-          <div class="card-body">
-            <h5 class="card-title text-sub-color">{{ $user->name }}</h5>
-            <p class="card-text">{{ $user->getProfileSummary('summary') }}</p>
 
-          </div>
-          <div class="card-body contact-bio d-flex flex-column flex-lg-row">
-            <span class="me-4 mb-2"><i class="far fa-map-marker-alt fa__icon-black me-2"></i>{{ $user->getLocation() }}</span>
-            @if (((bool)$user->verified))
-            <span class="me-4 mb-2 text-green-color"><i class="far fa-envelope fa__icon-black me-2"></i>{{ __('Email Verified') }}</span>
-            @endif
 
-            @if((bool)$user->is_immediate_available)
-              <span class="me-4 mb-2 text-green-color"><span class="icon-user-1-icon fs-18px me-2"></span>{{ __('Immediate Available') }}</span>
-            @else
-              <span class="me-4 mb-2 text-red-color"><span class="icon-user-1-icon fs-18px me-2"></span>{{ __('Not Immediate Available') }}</span>
-            @endif
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12 col-lg-8 col-sm-12">
-        <div class="summary-card row mx-0">
-          <div class="col-md-4 mb-4 d-flex gap-16">
-            <div class="summary-card__item-icon">
-              <div class="icon-gender icon-size-30"></div>
-            </div>
-            <div class="summary-card__item-content">
-              <p>{{__('Gender')}}</p>
-              <strong>{{$user->getGender('gender')}}</strong>
-            </div>
-          </div>
-          <div class="col-md-4 mb-4 d-flex gap-16">
-            <div class="summary-card__item-icon">
-              <div class="icon-birthday-cake icon-size-30"></div>
-            </div>
-            <div class="summary-card__item-content">
-              <p>{{__('Age')}}</p>
-              <strong>{{$user->getAge()}}</strong>
-            </div>
-          </div>
-          <div class="col-md-4 mb-4 d-flex gap-16">
-            <div class="summary-card__item-icon">
-              <div class="icon-wedding-rings icon-size-30"></div>
-            </div>
-            <div class="summary-card__item-content">
-              <p>{{__('Marital Status')}}</p>
-              <strong>{{$user->getMaritalStatus('marital_status')}}</strong>
-            </div>
-          </div>
-          <div class="col-md-4 mb-4 d-flex gap-16">
-            <div class="summary-card__item-icon">
-              <div class="icon-suicase icon-size-30"></div>
-            </div>
-            <div class="summary-card__item-content">
-              <p>{{__('Experience')}}</p>
-              <strong>{{$user->getJobExperience('job_experience')}}</strong>
-            </div>
-          </div>
-          <div class="col-md-4 mb-4 d-flex gap-16">
-            <div class="summary-card__item-icon">
-              <div class="icon-salary icon-size-30"></div>
-            </div>
-            <div class="summary-card__item-content">
-              <p>{{__('Current salary')}}</p>
-              <strong>{{ number_format($user->current_salary)}} {{$user->salary_currency}}</strong>
-            </div>
-          </div>
-          <div class="col-md-4 mb-4 d-flex gap-16">
-            <div class="summary-card__item-icon">
-              <div class="icon-salary icon-size-30"></div>
-            </div>
-            <div class="summary-card__item-content">
-              <p>{{__('Expected salary')}}</p>
-              <strong>{{number_format($user->expected_salary)}} {{$user->salary_currency}}</strong>
-            </div>
-          </div>
-        </div>
-        @if($user->video_link !=='' && null!==($user->video_link))
-        <div class="mb-5">
-          <h6 class="mb-4">Video</h6>
-          <iframe width="100%" height="400" src="{{$user->video_link}}" title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen></iframe>
-        </div>
-        @endif
-        <div class="mb-5">
-          <h6 class="mb-4">{{__('Experience')}}</h6>
-          <div class="" id="experience_div"></div>
-        </div>
+<div class="container company-content" bis_skin_checked="1">
+    <div class="user-account-section" bis_skin_checked="1">
 
-        <!-- Ẩn tính năng Portfolio HIRE-110 -->
-        <!-- <div class="mb-5">
-            <h3>{{__('Portfolio')}}</h3>
-            <div  id="projects_div"></div>
-        </div> -->
-      </div>
-      <div class="col-md-12 col-lg-4 col-sm-12">
-        <div class="mb-5">
-          <h6>{{__('Degree Level')}}</h6>
-            @foreach($user->profileEducation as $education)
-            <div class="form-card mb-3">
-              <h6 class="my-0">{{$education->degree_title}}</h6>
-              @php
-                $degree_type = ($education->Industries) ? ' | '.$education->Industries->industry : '';
-              @endphp
-              <p class="text-primary mb-2 font-weight-bold">{{$education->date_completion}} | {{$education->institution . $degree_type}}</p>
-              <h6 class="my-0">{{$education->degree}}</h6>
-              <p>{{$education->school}}</p>
+      <div class="box-profile-view" bis_skin_checked="1">
+        <div class="formpanel mt0" bis_skin_checked="1">
+          <div class="section-head" bis_skin_checked="1">
+            <h3 class="title">Thông tin ứng viên</h3>
+          </div>
+          <div class="section-body" bis_skin_checked="1">
+            <div class="row" bis_skin_checked="1">
+              <div class="col-lg-6 col-md-12 col-sm-12" bis_skin_checked="1">
+                <div class="info" bis_skin_checked="1">
+                  <div class="image" bis_skin_checked="1">
+                    {{ $user->printUserImage() }}
+                  </div>
+                  <ul class="info-list">
+                    <li>
+                      <p> <strong>Ứng viên:</strong></p>
+                      <p class="name"> <strong>{{ $user->name }}</strong></p>
+                    </li>
+                    <li>
+                      <p><strong>Tuổi</strong></p>
+                      <p>{{$user->getAge()}} </p>
+                    </li>
+                    <li>
+                      <p><strong>Quốc tịch:</strong></p>
+                      
+                      <p>Chưa cập nhật</p>
+                    </li>
+                    <li>
+                      <p><strong>Giới tính:</strong></p>
+                      <p>{{$user->getGender('gender')}} -   </p>
+                    </li>
+                    <li>
+                      <p><strong>Quốc gia:</strong></p>
+                      <p>Việt Nam</p>
+                    </li>
+                    <li>
+                      <p><strong>Tỉnh/Thành Phố:</strong></p>
+                      <p>Chưa cập nhật</p>
+                    </li>
+                    <li>
+                      <p><strong>Địa chỉ:</strong></p>
+                      <p> {{ $user->street_address ?  $user->street_address :"Chưa cập nhật" }}  </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-12 col-sm-12" bis_skin_checked="1">
+                
+              </div>
             </div>
-            @endforeach
-        </div>
+            <p class="title-flip">Mô tả bản thân</p>
+            <div class="p-4 fs-18px">
+              {{ $user->getProfileSummary('summary') }}
+            </div>
 
-        <div class="mb-5">
-            @php($jobSkills = \App\JobSkill::where('lang',\App::getLocale())->pluck('job_skill','job_skill_id'))
-            @php($jobExperiences = \App\JobExperience::where('lang', \App::getLocale())->pluck('job_experience','job_experience_id'))
-            <h6>{{__('Skills')}}</h6>
-            @foreach($user->profileSkills as $skill)
-            <div class="form-card d-flex justify-content-between mb-3">
-              <p class="text-primary mb-2 font-weight-bold">{{ $jobSkills[$skill->job_skill_id] ?? ''}}</p>
-              <p>{{$jobExperiences[$skill->job_experience_id] ?? ''}} {{$skill->job_experience_id}}/5</p>
+            <p class="title-flip">Thông tin nghề nghiệp</p>
+
+
+            <div class="job-information" bis_skin_checked="1">
+              <ul class="information-list">
+                <li>
+                  <p> <strong>Năm kinh nghiệm:</strong></p>
+                  <p>{{$user->getJobExperience('job_experience')}}</p>
+                </li>
+                <li>
+                  <p> <strong>Bằng cấp cao nhất:</strong></p>
+                  <p> Chưa có thông tin</p>
+                </li>
+                <li>
+                  <p> <strong>Lương hiện tại</strong></p>
+                  <p>{{ number_format($user->current_salary)}}</p>
+                </li>
+                <li>
+                  <p> <strong>Mức lương mong muốn:</strong></p>
+                  <p>{{number_format($user->expected_salary)}} </p>
+                </li>
+                <li>
+                  <p> <strong>Ngành nghề:</strong></p>
+                  <p>{{$user->getIndustry('industry')}}</p>
+                </li>
+                <li>
+                  <p> <strong>Địa điểm:</strong></p>
+                  <p>Chưa cập nhật</p>
+                </li>
+                <li>
+                  <p> <strong>Hình thức:</strong></p>
+                  <p>Chưa cập nhật</p>
+                </li>
+                <li>
+                  <p> <strong>Ngày tạo:</strong></p>
+                  <p>{{ date('d-m-Y', strtotime($user->created_at)) }}</p>
+                </li>
+                <li>
+                  <p> <strong>Cập nhật:</strong></p>
+                  <p>{{ date('d-m-Y', strtotime($user->updated_at)) }}</p>
+                </li>
+              </ul>
+
             </div>
-            @endforeach
-        </div>
-        <div class="mb-5">
-            @php($jobLanguages = \App\Helpers\DataArrayHelper::languagesArray())
-            @php($jobLanguageLevels = \App\LanguageLevel::all()->pluck('language_level','language_level_id'))
-          <h6>{{__('Languages')}}</h6>
-            @foreach($user->profileLanguages as $language)
-            <div class="form-card d-flex justify-content-between mb-3">
-              <p class="text-primary mb-2 font-weight-bold"> {{$jobLanguages[$language->language_id]?? ''}}</p>
-              <p>{{$jobLanguageLevels[$language->language_level_id] ?? ''}}</p>
-            </div>
-            @endforeach
+            <!-- <p class="title-flip">NỘI DUNG HỒ SƠ</p> -->
+          </div>
 
         </div>
       </div>
     </div>
-  </section>
+  </div>
 
 @push('styles')
 <style type="text/css">
     .formrow iframe {
         height: 78px;
     }
+    .user-account-section .action {
+      background: #fff1f4;
+      padding: 8px 20px;
+    }
+    .user-account-section .action .action-list {
+      display: flex;
+      list-style-type: none;
+      margin-bottom: 0;
+      padding-left: 0;
+      -webkit-box-pack: end;
+      -ms-flex-pack: end;
+      justify-content: flex-end;
+    }
+    .box-profile-view .section-head .title {
+      color: var(--bs-primary);
+      font-size: 16px;
+      font-weight: 700;
+      text-transform: uppercase;
+    }
+    .box-profile-view .section-body .info {
+      display: flex;
+    }
+    .box-profile-view .section-body .info .image {
+      -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 160px;
+    height: 160px;
+    margin-top: 20px;
+    margin-right: 40px;
+    margin-left: 0;
+    overflow: hidden;
+    }
+    .box-profile-view .section-body .info .image img {
+      width: 100%;
+      height: 100%;
+    }
+
+    .box-profile-view .section-body .info .info-list {
+      margin-top: 25px;
+      list-style-type: none;
+    }
+    .box-profile-view .section-body .info-list li {
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+    }
+
+    .box-profile-view .section-body .info-list li p:first-child {
+      width: 200px;
+      min-width: 200px;
+      padding-right: 10px;
+    }
+    .box-profile-view .section-body .info-list li p {
+      color: #182642;
+      font-size: 17;
+      padding: 1px;
+
+    }
+    .box-profile-view .title-flip {
+      margin-top: 15px;
+      padding: 8px 20px;
+      background: #fff1f4;
+      color: #172642;
+      font-size: 16px;
+      font-weight: 700;
+      text-transform: uppercase;
+    }
+    .box-profile-view .job-information {
+      margin-top: 15px;
+    padding: 0 15px;
+    }
+    .box-profile-view .job-information .information-list {
+      display: flex;
+      flex-wrap: wrap;
+      list-style-type: none;
+    }
+    .box-profile-view .job-information .information-list li {
+      flex: 0 0 50%;
+      max-width: 50%;
+      width: 100%;
+      padding-right: 10px;
+      display: flex;
+    }
+    .box-profile-view .job-information .information-list li p:first-child {
+      width: 200px;
+      min-width: 200px;
+      padding-right: 5px
+    }
+    
+
+
 </style>
 @endpush
 @push('scripts') 

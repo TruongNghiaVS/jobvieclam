2663fc1597466ef062486e4c0260dfc9943abb3a
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2024-01-12 16:07:19 +0700

    add bannner

--- resources/views/templates/vietstar/includes/advertising_banner.blade.php
+++ resources/views/templates/vietstar/includes/advertising_banner.blade.php
@@ -29,7 +29,7 @@
                             if (element.postion.startsWith('2.')) {
                                 
                                 $("#listbannerSide").append(`
-                                    <div class=" col-lg col-sm-12 col-md-12">
+                                    <div class="col-lg col-sm-12 col-md-12">
                                         <div class="item">
                                             <div class="image loadAds">
                                                 <a href="#">

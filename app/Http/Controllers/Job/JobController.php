c0f8a4048f0a9387da9a8d74c6c3c23ab4cf6171
nghia <nghiait06@gmail.com>
2024-01-19 15:20:40 +0700

    update job

--- app/Http/Controllers/Job/JobController.php
+++ app/Http/Controllers/Job/JobController.php
@@ -604,46 +604,7 @@ class JobController extends Controller
     }
 
 
-    public function changeStatus(Request $request)
-    {
-        $id =$request->intput("id");
-        $status =$request->intput("status");
-        if($id)
-        {
-
-        }
-        else 
-        {
-            $id =-1;
-        }
-
-        $itemError = new stdClass();
-        $itemError->success =true;
-        $itemError->message ="Yêu cầu thành công";
-        $job = Job::findOrFail($id);
-        if($job)
-        {
-
-        }
-        else 
-        {
-            $itemError->success =false;
-            $itemError->message ="Yêu cầu thất bại";
-            return response()->json([
-                'success'=>false,
-                'error'=> $itemError ], 200);
-        }
-        
-        if($status =="4")
-        {
-            $job->status ="1";
-        }
-        $job->save();
-        return response()->json([
-            'success'=>true,
-            'error'=> $itemError ], 200);
-       
-    }
+  
     public function postApplyJob(Request $request, $job_slug)
     {
         $user = Auth::user();

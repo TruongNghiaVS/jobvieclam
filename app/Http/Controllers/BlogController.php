81e53d8401d5d39694c10f072f838b43d5865e6b
nghia <nghiait06@gmail.com>
2024-01-22 13:29:45 +0700

    update code

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -82,7 +82,8 @@ class BlogController extends Controller
         $data['category'] = $category;
         $data['blogs_categories'] = Blog_category::where("id", $category->id)->where("typePost",0)->get();
         $data['blogs'] = Blog::whereRaw("FIND_IN_SET('$category->id',cate_id)")
-                        ->where('lang', 'like', \App::getLocale())->orderBy('id', 'DESC')->paginate(10);
+                        ->where('lang', 'like', \App::getLocale())->orderBy('id', 'DESC')
+                        ->paginate(100);
         
       
         return view(config('app.THEME_PATH').'.blog_categories_details', compact('data'));

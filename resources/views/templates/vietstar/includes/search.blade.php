33b29ac3bd742456d0df5dd45182fd92112ba352
Thanh Minh & Hoan <90053384+thanhminh1510@users.noreply.github.com>
2024-01-30 16:50:44 +0700

    check ui

--- resources/views/templates/vietstar/includes/search.blade.php
+++ resources/views/templates/vietstar/includes/search.blade.php
@@ -5,7 +5,7 @@
                 @php
                 $sliders = \App\Slider::where("type",0)->select([
                 'sliders.id', 'sliders.slider_id',
-                'sliders.is_active','sliders.used_for','sliders.slider_image','sliders.slider_image_mobile'
+                'sliders.is_active','sliders.used_for','sliders.slider_image','sliders.slider_image_mobile','sliders.linkImage3'
                 ])->sorted()->get();
                 @endphp
                 <div class="swiper-wrapper">
@@ -15,6 +15,8 @@
                             <div class="swiper-slide p-0">
                                 <div class="is-pc bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image}});"></div>
                                 <div class="is-sp bg-slider" style="background-image: url({{'slider_images/'.$slider->slider_image_mobile}});"></div>
+                                <div class="is-pc-sm bg-slider" style="background-image: url({{'slider_images/'.$slider->linkImage3}});"></div>
+                            
                             </div>
                         @endif
                     @endforeach
@@ -108,13 +110,13 @@
                     <div class="col-lg-5  shadow p-4 search-form-box pb-1">
 
                         <div class="row">
-                            <div class="col-lg-12">
+                            <div class="col-lg-6 col-sm-12 col-md-6">
                                 <h3 class="header-bar__title mb-3" style="max-width: 100%; width: 100%">Tìm Kiếm Cơ Hội Việc Làm
                                 </h3>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-lg-12">
+                            <div class="col-lg-12 col-sm-12 col-md-12">
                                 <div class="form-group">
                                     <i class="fa-solid fa-magnifying-glass"></i>
                                     <input type="text" class="form-control search-input shadow-sm" style="width: 100%" id="search" name="search" placeholder="{{__('Job search')}}">
@@ -123,20 +125,20 @@
                             </div>
                         </div>
                         <div class="row form-group-search-box">
-                            <div class="col-sm-6">
+                            <div class="col-lg-6 col-sm-12 col-md-6">
                                 <div class="form-group form-group-search" id="city_dd">
                                     {!! Form::select('city_id[]', ['' => __('Select City')]+$citiesArray, Request::get('city_id',
                                     null), array('class'=>'form-control form-select shadow-sm', 'id'=>'city_id')) !!}
                                 </div>
                             </div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-6 col-sm-12 col-md-6">
                                 <div class="form-group form-group-search" id="job_type_dd">
                                     {!! Form::select('job_type_id[]', ['' => __('Select Job Type')] + $jobtypesArray,
                                     Request::get('job_type_id', null), array('class'=>'form-control form-select shadow-sm',
                                     'id'=>'job_type_id')) !!}
                                 </div>
                             </div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-6 col-sm-12 col-md-6">
                                 <div class="form-group form-group-search" id="career_level_dd">
                                 
                                     {!! Form::select('degree_level_id[]',['' => __('Academic level')] + $degreeLevelArray,
@@ -144,7 +146,7 @@
                                     'id'=>'degree_level_id')) !!}
                                 </div>
                             </div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-6 col-sm-12 col-md-6">
                                 <div class="form-group form-group-search" id="degree_leve_dd">
                                     {!! Form::select('industry_id[]', ['' => __('Select Industry')] + $industryArray,
                                     Request::get('industry_id', null), array('class'=>'form-control form-select shadow-sm',
@@ -154,7 +156,7 @@
                         </div>
 
                         <div class="row">
-                            <div class="col-lg-12">
+                            <div class="col-lg-6 col-sm-12 col-md-12">
                                 <div class="advance-search row">
                                     <div class="advance-search__reset col-lg-6">Đặt Lại</div>
                                     <div class="col-lg-6 advance-search__box">
@@ -162,6 +164,9 @@
                                         <div class="advance-search__close" onClick="closesearchbox()"><i class="fa-solid fa-angle-up"></i> Thu nhỏ</div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-12 col-md-12">
+
                                 <div class="action-search-form">
                                     <button type="submit" class="btn btn-primary mb-3 btn-submit-search">{{__('Search')}}</button>
                                 </div>

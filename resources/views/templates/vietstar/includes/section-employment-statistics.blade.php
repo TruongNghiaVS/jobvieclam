fee5188a5314378ea1deefb76386861c4be532bd
minhdev <minhdev@gmail.com>
2024-03-07 09:34:12 +0700

    css

--- resources/views/templates/vietstar/includes/section-employment-statistics.blade.php
+++ resources/views/templates/vietstar/includes/section-employment-statistics.blade.php
@@ -1,4 +1,4 @@
-<section class="section-employment-statistics" id= "flash-badge-banner"  style="background-image: url({{ asset('/vietstar/imgs') }}/background_main.png);">
+{{--<section class="section-employment-statistics" id= "flash-badge-banner"  style="background-image: url({{ asset('/vietstar/imgs') }}/background_main.png);">
         <div class="cbox-impressive-numbers__item--wrapper">
         <div class="container">
         <h2 class="section-title section-title-decorated section-title-decorated-white">Con số ấn tượng</h2>
@@ -85,22 +85,24 @@
                 </div>
             </div>
         </div>
+    </div>
+
+</section>--}}
+
+
+
+
+
+
+<section class="section-employment-statistics" id="flash-badge-banner" style="background-image: url({{ asset('/vietstar/imgs') }}/background_main.png);">
+    <div class="container">    
+        <div class="cbox-impressive-numbers__item--wrapper">
+            <h2 class="section-title section-title-decorated section-title-decorated-white">Con số ấn tượng</h2>
+            <div class="section-subtitle">Công ty Cổ phần Tập đoàn Vietstar ( VIETSTAR GROUP JOINT STOCK COMPANY) là một trong những công ty uy tín tại Việt Nam trong lĩnh vực cung cấp giải pháp xử lý trong lĩnh vực cung ứng nhân sự đa ngành nghề.</div>
         </div>
-    
 
-
-
-
-
-
-
-
-
-
-
-
-
-            <!-- <div class="col-md-6 col-lg-4 col-sm-12 mb-4">
+        <div class="row">
+            <div class="col-md-6 col-lg-4 col-sm-12 mb-4">
                 <div class="statistic">
                     <div class="icon">
                         <i class="fa-solid fa-suitcase"></i>
@@ -165,18 +167,143 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-lg-4 col-sm-12 mb-4">
-                <div class="statistic">
-                    <div class="icon">
-                        <i class="fa-solid fa-users-rays"></i>
-                    </div>
-                    <div class="content">
-                        <h3 class="num">5.000.000+</h3>
-                        <div class="desc">
-                            Ứng viên tiềm năng, trong đó có 60% là ứng viên có kinh nghiệm từ 2 năm trở lên.
+                <div class="col-md-6 col-lg-4 col-sm-12 mb-4">
+                    <div class="statistic">
+                        <div class="icon">
+                            <i class="fa-solid fa-users-rays"></i>
+                        </div>
+                        <div class="content">
+                            <h3 class="num">5.000.000+</h3>
+                            <div class="desc">
+                                Ứng viên tiềm năng, trong đó có 60% là ứng viên có kinh nghiệm từ 2 năm trở lên.
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div> -->
-       
+            </div>
+        </div>
+    </div>
 </section>
+
+
+
+@push('styles')
+<style>    
+   
+    section#flash-badge-banner {
+        position: relative;
+        width: 100%;
+        padding: 20px 0 0px 0;
+        margin-bottom:10px;
+        background-position: 50%;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+
+    .cbox-impressive-numbers__item--wrapper {
+        color: #fff;
+        font-size: 28px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 28px;
+        
+    }
+
+    #flash-badge-banner .cbox-impressive-numbers__item--wrapper .section-title {
+        align-items: center;
+        color: white;
+        -moz-column-gap: 24px;
+        column-gap: 24px;
+        display: flex;
+        font-size: 25px;
+        font-weight: 700;
+        justify-content: center;
+        line-height: 25px;
+        margin: 0 0 10px;
+        position: relative;
+        text-align: center;
+        width: 100%;
+    }
+
+    #flash-badge-banner .cbox-impressive-numbers__item--wrapper .section-subtitle {
+        color: #fff;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 20px;
+        margin-bottom: 10px;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 1100px;
+        text-align: center;
+        width: 100%
+    }
+
+    .section-employment-statistics .statistic {
+        position: relative;
+        background: #ffffff;
+        border: 1px solid #d9dbe9;
+        -webkit-box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+        box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1);
+        border-radius: 6px;
+        height: 100%;
+        -webkit-box-pack: justify;
+        -ms-flex-pack: justify;
+        justify-content: space-between;
+        padding: 15px 20px 15px 80px;
+    }
+
+    .section-employment-statistics .statistic .icon {
+        position: absolute;
+        top: 20px;
+        left: 20px;
+        width: 46px;
+        height: 46px;
+        background-color: rgba(152, 27, 30, 0.12);
+        border: 1px solid #981B1E;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        border-radius: 3px;
+        margin-bottom: 10px
+    }
+
+    .section-employment-statistics .statistic .icon .iconmoon {
+        font-size: 24px
+    }
+
+    .section-employment-statistics .statistic .icon i,.section-employment-statistics .statistic .icon span {
+        color: var(--bs-primary)
+    }
+
+    .section-employment-statistics .statistic .content {
+        padding: 0
+    }
+
+    .section-employment-statistics .statistic .content .num {
+        font-size: 25px;
+        color: var(--bs-primary);
+        line-height: 1.2;
+        margin-bottom: 10px
+    }
+
+    .section-employment-statistics .statistic .content .desc {
+        font-size: 15px;
+        line-height: 1.4;
+        -webkit-line-clamp: 4;
+        -webkit-box-orient: vertical;
+        display: -webkit-box;
+        overflow: hidden;
+
+    }
+
+
+</style>
+@endpush
